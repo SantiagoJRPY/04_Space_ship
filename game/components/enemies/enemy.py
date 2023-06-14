0b273655e1b297c07ca49e809a4a2ff11833807d
Santiago <Santiago.Jimenez@fundacion-jala.org>
2023-06-14 13:38:00 -0500

    se agregaron velocidades a las naves enemigas

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -8,12 +8,14 @@ class Enemy(Sprite):
     Y_POS = 20
     X_POS_LIST = [50,100,150,200,250,300,350,400,450,500,550]
     SPEED_X = 5
-    SPEED_Y = 1
+    SPEED_Y = [1,2,3]
     MOV_X = {0:'left', 1:'right'}
+    enemies = [ENEMY_1, ENEMY_2, ENEMY_3]
+    MOV_LEFT = 0
+    MOV_RIGHT = 0
 
     def __init__(self):
-        enemies = [ENEMY_1, ENEMY_2, ENEMY_3]
-        self.image = random.choice(enemies)
+        self.image = random.choice(self.enemies)
         self.image = pygame.transform.scale(self.image, (40,60))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS_LIST[random.randint(0,10)]
@@ -21,7 +23,7 @@ class Enemy(Sprite):
         self.type = 'enemy'
 
         self.speed_x = self.SPEED_X
-        self.speed_y = self.SPEED_Y
+        self.speed_y = random.choice(self.SPEED_Y)
         self.movement_x = self.MOV_X[random.randint(0,1)]
         self.move_x_for = random.randint(30,100)
         self.index = 0
@@ -29,7 +31,7 @@ class Enemy(Sprite):
 
     def change_movement_x(self):
         self.index += 1
-        if (self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH -40):
+        if (self.index >= self.move_x_for and self.movement_x == 'right') or (self.rect.x >= SCREEN_WIDTH):
             self.movement_x = 'left'
         elif (self.index >= self.move_x_for and self.movement_x == 'left') or (self.rect.x <= 10):
             self.movement_x = 'right'
@@ -44,9 +46,11 @@ class Enemy(Sprite):
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x
             self.change_movement_x()
+            self.MOV_LEFT += 1
         else:
             self.rect.x += self.speed_x
             self.change_movement_x()
+            self.MOV_RIGHT += 1
         if self.rect.y >= SCREEN_HEIGHT:
             ships.remove(self)
 
