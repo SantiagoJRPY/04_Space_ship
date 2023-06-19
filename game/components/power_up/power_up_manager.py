7ef89991f12b7a33f77649e25e73ba1e4691b7b1
Santiago <Santiago.Jimenez@fundacion-jala.org>
2023-06-19 14:51:17 -0500

    Arreglando errores

--- game/components/power_up/power_up_manager.py
+++ game/components/power_up/power_up_manager.py
@@ -15,7 +15,7 @@ class PowerUpManager:
         self.duration = random.randint(3,5)
 
     def generate_power_up(self):
-        power_up = Burst()
+        power_up = random.choice(self.appear_power_up)
         self.when_appears += random.randint(5000, 10000)
         self.power_ups.append(power_up)
 
@@ -25,7 +25,7 @@ class PowerUpManager:
         if len(self.power_ups) == 0 and current_time >= self.when_appears:
             self.generate_power_up()
 
-        for power_up in self.appear_power_up:
+        for power_up in self.power_ups:
             power_up.update(game.game_speed, self.power_ups)
             if game.player.rect.colliderect(power_up):
                 power_up.start_time = pygame.time.get_ticks()
@@ -34,7 +34,7 @@ class PowerUpManager:
                 game.player.power_time_up = power_up.start_time + (self.duration * 1000)
                 game.player.set_image((65,75), SPACESHIP_SHIELD)
                 self.power_ups.remove(power_up)
-
+            
 
     def draw(self,screen):
         for power_up in self.power_ups:
